c32c8de482a26d9c577cdbe05a2f909011840885
Chad <chadndonahue@gmail.com>
2022-10-17 15:52:36 -0700

    Added a card with contact info and got rid of margins on Row

--- reactportfolio/src/components/About.js
+++ reactportfolio/src/components/About.js
@@ -1,3 +1,18 @@
 import React from "react";
 
-export default function About() {}
+import '../App.css'
+
+export default function About() {
+    return (
+<>
+<div class ="jumbotron">
+  <h1>Hello, thank you for viewing my portfolio.</h1>
+  <p>
+    My name is Layne Donahue, I am a full stack web developer from Upstate New York.  Recently, I made the move to Los Angeles in search of better opportunities for work.
+    With each day, I grow and learn as a person and developer.  I believe that no task is below me or too great for me, as everything in life is a learning opportunity.
+    I plan to continue to grow and become a better developer.
+  </p>
+</div>;
+</>
+);
+}
--- reactportfolio/src/components/Contact.js
+++ reactportfolio/src/components/Contact.js
@@ -1,3 +1,30 @@
 import React from "react";
 
-export default function Contact() {}
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row'
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import '.././App.css'
+
+export default function Contact() {
+    
+  return (
+    <>
+    <Row className="justify-content-center">
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Contact Info</Card.Title>
+        <Card.Text>
+         The simplest way to contact me would be through email or phone.
+        </Card.Text>
+        <ListGroup variant="flush">
+        <ListGroup.Item>Email: chadndonahue@gmail.com</ListGroup.Item>
+        <ListGroup.Item>Phone: (518)572-2973</ListGroup.Item>
+      </ListGroup>
+      </Card.Body>
+    </Card>
+    </Row>
+    </>
+  );
+}
+
