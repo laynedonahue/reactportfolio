70634c9ce5ed12321736a6301b0fa93476d21c03
Chad <chadndonahue@gmail.com>
2022-10-17 13:40:46 -0700

    added bootstrap

--- reactportfolio/src/components/Navbar.js
+++ reactportfolio/src/components/Navbar.js
@@ -1,1 +1,28 @@
+import React from "react";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Navbar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+            <Nav.Link href="#Projects">Projects</Nav.Link>
+            <Nav.Link href="#Skills">Skills</Nav.Link>
+            <Nav.Link href="#Testimonials">Testimonials</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </>
+  );
+}
+
+
+
 export default function Navbar() {}
