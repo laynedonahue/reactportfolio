aead37d9aefc93ea78b2b0f4adbbbc3d4fbf9d8d
Chad <chadndonahue@gmail.com>
2022-10-19 15:14:37 -0700

    added a container to herotxt so text is easier to read

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 import '../App.css'
 
@@ -8,11 +9,13 @@ export default function About() {
 <div className= "jumbotron" id='About'>
   <h1>Hello, welcome!</h1>
   <img src='/images/hero.png' id='heroImg'></img>
-  <p>
-    My name is Layne Donahue, I am a full stack web developer from Upstate New York.  Recently, I made the move to Los Angeles in search of better opportunities for work.
-    With each day, I grow and learn as a person and developer.  I believe that no task is below me or too great for me, as everything in life is a learning opportunity.
-    I plan to continue to grow and become a better developer.
-  </p>
+    <Container>
+      <p className="aboutTxt">
+        My name is Layne Donahue, I am a full stack web developer from Upstate New York.  Recently, I made the move to Los Angeles in search of better opportunities for work.
+        With each day, I grow and learn as a person and developer.  I believe that no task is below me or too great for me, as everything in life is a learning opportunity.
+        I plan to continue to grow and become a better developer.
+      </p>
+    </Container>
 </div>;
 </>
 );
